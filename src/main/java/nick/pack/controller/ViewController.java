0b803a5c446b3f73a2ce5849a04ba1a1b950c198
andrey <76613379+Andreynikonov23@users.noreply.github.com>
2022-12-18 17:24:06 +0300

    session problem

--- src/main/java/nick/pack/controller/ViewController.java
+++ src/main/java/nick/pack/controller/ViewController.java
@@ -1,11 +1,20 @@
 package nick.pack.controller;
 
+import nick.pack.model.User;
 import nick.pack.service.ReviewService;
 import nick.pack.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.session.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.extras.springsecurity5.util.SpringSecurityContextUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class ViewController {
@@ -17,6 +26,8 @@ public class ViewController {
     @GetMapping("/")
     public String test(Model model){
         model.addAttribute("reviews", reviewService.findByAll());
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("session", auth);
         return "index";
     }
 }
