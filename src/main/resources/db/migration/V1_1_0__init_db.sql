b4e432fa77401ec52f682932142e742c4bb2f11a
Andrey <xxxkeep3rxxx@gmail.com>
2023-10-24 16:04:59 +0300

    sql migrate fix

--- src/main/resources/db/migration/V1_1_0__init_db.sql
+++ src/main/resources/db/migration/V1_1_0__init_db.sql
@@ -1,81 +1,261 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
 
-drop sequence if exists comments_id_seq;
-drop sequence if exists country_id_seq;
-drop sequence if exists ratings_id_seq;
-drop sequence if exists reviews_id_seq;
-drop sequence if exists role_id_seq;
-drop sequence if exists status_id_seq;
-drop sequence if exists users_id_seq;
+SET default_tablespace = '';
 
-create table if not exists comments (
-    id  serial not null,
-    comment text,
-    date date,
-    answer int4,
-    review_id int4,
-    user_id int4,
-    primary key (id));
-create table if not exists country (
-    id  serial not null,
-    country_name varchar(20),
-    primary key (id));
- create table if not exists ratings (
-    id serial not null,
-    rating int4,
-    review int4,
-    user_id int4,
-    primary key (id));
-create table if not exists reviews (
-    id  serial not null,
-    cast_names varchar(500),
-    director varchar(60),
-    film_name varchar(255),
-    name varchar(50),
-    poster varchar(300),
-    text text,
-    trailer_url varchar(100),
-    year int4,
-    country_id int4,
-    user_id int4,
-    primary key (id));
-create table if not exists role (
-    id  serial not null,
-    name varchar(10),
-    primary key (id));
-create table if not exists status (
-    id  serial not null,
-    status varchar(7),
-    primary key (id));
-create table if not exists users (
-    id  serial not null,
-    email varchar(40),
-    login varchar(25),
-    nickname varchar(50),
-    password varchar(100),
-    photo varchar(70),
-    role_id int4,
-    status_id int4,
-    primary key (id));
-ALTER TABLE ONLY comments ALTER COLUMN id SET DEFAULT nextval('comments_id_seq'::regclass);
-ALTER TABLE ONLY country ALTER COLUMN id SET DEFAULT nextval('country_id_seq'::regclass);
-ALTER TABLE ONLY ratings ALTER COLUMN id SET DEFAULT nextval('ratings_id_seq'::regclass);
-ALTER TABLE ONLY reviews ALTER COLUMN id SET DEFAULT nextval('reviews_id_seq'::regclass);
-ALTER TABLE ONLY role ALTER COLUMN id SET DEFAULT nextval('role_id_seq'::regclass);
-ALTER TABLE ONLY status ALTER COLUMN id SET DEFAULT nextval('status_id_seq'::regclass);
-ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
-SELECT pg_catalog.setval('comments_id_seq', 1, true);
-SELECT pg_catalog.setval('country_id_seq', 1, true);
-SELECT pg_catalog.setval('ratings_id_seq', 1, true);
-SELECT pg_catalog.setval('reviews_id_seq', 1, true);
-SELECT pg_catalog.setval('role_id_seq', 1, true);
-SELECT pg_catalog.setval('status_id_seq', 1, true);
-SELECT pg_catalog.setval('users_id_seq', 1, true);
-alter table comments add constraint answer foreign key (answer) references comments on update cascade on delete cascade;
-alter table comments add constraint reviews_id foreign key (review_id) references reviews on update cascade on delete cascade;
-alter table comments add constraint comments_users_id_fkey foreign key (user_id) references users on update cascade on delete cascade;
-alter table ratings add constraint review foreign key (review) references reviews on update cascade on delete cascade;
-alter table ratings add constraint "user" foreign key (user_id) references users on update cascade on delete cascade;
-alter table reviews add constraint reviews_country_id_fkey foreign key (country_id) references country on update cascade on delete cascade;
-alter table reviews add constraint reviews_users_id_fkey foreign key (user_id) references users on update cascade on delete cascade;
-alter table users add constraint users_role_id_fkey foreign key (role_id) references role on update cascade on delete cascade;
-alter table users add constraint users_status_id_fkey foreign key (status_id) references status on update cascade on delete cascade;
+SET default_table_access_method = heap;
+
+CREATE TABLE public.comments (
+         id integer NOT NULL,
+         user_id integer NOT NULL,
+         comment text NOT NULL,
+         answer integer,
+         review_id integer NOT NULL,
+         date timestamp without time zone
+);
+
+
+ALTER TABLE public.comments OWNER TO postgres;
+CREATE SEQUENCE public.comments_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.comments_id_seq OWNER TO postgres;
+
+ALTER SEQUENCE public.comments_id_seq OWNED BY public.comments.id;
+
+CREATE TABLE public.country (
+        id integer NOT NULL,
+        country_name character varying(20)
+);
+
+ALTER TABLE public.country OWNER TO postgres;
+
+CREATE SEQUENCE public.country_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.country_id_seq OWNER TO postgres;
+
+ALTER SEQUENCE public.country_id_seq OWNED BY public.country.id;
+
+CREATE TABLE public.ratings (
+        id integer NOT NULL,
+        rating integer NOT NULL,
+        user_id integer NOT NULL,
+        review integer NOT NULL
+);
+
+
+ALTER TABLE public.ratings OWNER TO postgres;
+
+CREATE SEQUENCE public.ratings_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.ratings_id_seq OWNER TO postgres;
+
+ALTER SEQUENCE public.ratings_id_seq OWNED BY public.ratings.id;
+
+CREATE TABLE public.reviews (
+        id integer NOT NULL,
+        name character varying(50) NOT NULL,
+        trailer_url character varying(100),
+        poster character varying(300),
+        film_name character varying(50) NOT NULL,
+        year integer,
+        director character varying(60),
+        cast_names character varying(500),
+        text text,
+        country_id integer,
+        user_id integer NOT NULL
+);
+
+
+ALTER TABLE public.reviews OWNER TO postgres;
+
+CREATE SEQUENCE public.reviews_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.reviews_id_seq OWNER TO postgres;
+
+
+ALTER SEQUENCE public.reviews_id_seq OWNED BY public.reviews.id;
+
+CREATE TABLE public.role (
+                             id integer NOT NULL,
+                             name character varying(10) NOT NULL
+);
+
+
+ALTER TABLE public.role OWNER TO postgres;
+
+CREATE SEQUENCE public.role_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.role_id_seq OWNER TO postgres;
+
+
+ALTER SEQUENCE public.role_id_seq OWNED BY public.role.id;
+
+
+CREATE TABLE public.status (
+           id integer NOT NULL,
+           status character varying(7) NOT NULL
+);
+
+
+ALTER TABLE public.status OWNER TO postgres;
+
+CREATE SEQUENCE public.status_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.status_id_seq OWNER TO postgres;
+
+ALTER SEQUENCE public.status_id_seq OWNED BY public.status.id;
+
+CREATE TABLE public.users (
+              id integer NOT NULL,
+              login character varying(25) NOT NULL,
+              password character varying(100) NOT NULL,
+              role_id integer NOT NULL,
+              status_id integer NOT NULL,
+              nickname character varying(50) NOT NULL,
+              photo character varying(70),
+              email character varying(40)
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_id_seq OWNER TO postgres;
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+ALTER TABLE ONLY public.comments ALTER COLUMN id SET DEFAULT nextval('public.comments_id_seq'::regclass);
+
+
+ALTER TABLE ONLY public.country ALTER COLUMN id SET DEFAULT nextval('public.country_id_seq'::regclass);
+
+
+ALTER TABLE ONLY public.ratings ALTER COLUMN id SET DEFAULT nextval('public.ratings_id_seq'::regclass);
+
+
+ALTER TABLE ONLY public.reviews ALTER COLUMN id SET DEFAULT nextval('public.reviews_id_seq'::regclass);
+
+
+ALTER TABLE ONLY public.role ALTER COLUMN id SET DEFAULT nextval('public.role_id_seq'::regclass);
+
+
+ALTER TABLE ONLY public.status ALTER COLUMN id SET DEFAULT nextval('public.status_id_seq'::regclass);
+
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+SELECT pg_catalog.setval('public.comments_id_seq', 1, false);
+SELECT pg_catalog.setval('public.country_id_seq', 1, false);
+SELECT pg_catalog.setval('public.ratings_id_seq', 1, false);
+SELECT pg_catalog.setval('public.reviews_id_seq', 1, false);
+SELECT pg_catalog.setval('public.role_id_seq', 1, false);
+SELECT pg_catalog.setval('public.status_id_seq', 1, false);
+SELECT pg_catalog.setval('public.users_id_seq', 1, false);
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT comments_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.country
+    ADD CONSTRAINT country_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.ratings
+    ADD CONSTRAINT ratings_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT reviews_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.role
+    ADD CONSTRAINT role_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.status
+    ADD CONSTRAINT status_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT answer FOREIGN KEY (answer) REFERENCES public.comments(id) ON UPDATE CASCADE ON DELETE CASCADE NOT VALID;
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT country_id_fkey FOREIGN KEY (country_id) REFERENCES public.country(id) ON UPDATE CASCADE ON DELETE CASCADE NOT VALID;
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT review_id_fkey FOREIGN KEY (review_id) REFERENCES public.reviews(id) ON UPDATE CASCADE ON DELETE CASCADE NOT VALID;
+
+ALTER TABLE ONLY public.ratings
+    ADD CONSTRAINT review_id_fkey FOREIGN KEY (review) REFERENCES public.reviews(id) ON UPDATE CASCADE ON DELETE CASCADE NOT VALID;
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT role_id_fkey FOREIGN KEY (role_id) REFERENCES public.role(id) ON UPDATE CASCADE ON DELETE CASCADE NOT VALID;
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT status_id FOREIGN KEY (status_id) REFERENCES public.status(id) ON UPDATE CASCADE ON DELETE CASCADE NOT VALID;
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT VALID;
+
+ALTER TABLE ONLY public.ratings
+    ADD CONSTRAINT user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT VALID;
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT VALID;
